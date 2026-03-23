900d0d13d77b377872fe09ba4db0b81444242c1e
Slava Kochnev <sl.kochnev@mail.ru>
2026-03-24 00:19:30 +0500

    Начинаем программировать

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainPage from '../../pages/main-page/main-page';
+
+interface AppProps {
+  offersCount: number;
+}
+
+function App({ offersCount } : AppProps) {
+  return (
+    <MainPage offersCount={offersCount}/>
+  );
+}
+
+export default App;
